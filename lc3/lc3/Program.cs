0771fc4ac5b55510fdf0c46f9883906fddc56482
dkohn <douglasjkohn@gmail.com>
2022-08-07 15:47:55 -0500

    Added CSharp Solution

--- lc3/lc3/Program.cs
+++ lc3/lc3/Program.cs
@@ -1,5 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Leetcode solution for LC3
+// Sliding window
+// O(n)
 
 
 
+ int LengthOfLongestSubstring(string s)
+{
+    HashSet<char> charSet = new HashSet<char>();
+    int left = 0;
+    int result = 0;
+    for (int right = 0; right < s.Length; right++)
+    {
+        while (charSet.Contains(s[right]))
+        {
+            charSet.Remove(s[left]);
+            left += 1;
+        }
+        charSet.Add(s[right]);
+        result = Math.Max(result, right - left + 1);
+    }
 
+    return result;  
+}
+
+Console.WriteLine(LengthOfLongestSubstring("abcabcbb"));
