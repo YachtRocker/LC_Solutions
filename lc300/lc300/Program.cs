ae8c0e23a486a4b8371257445cc0ace49cd8a9ee
dkohn <douglasjkohn@gmail.com>
2022-08-08 12:59:51 -0500

    lc 300 Longest Increasing Subsequence Solution

---
+++ lc300/lc300/Program.cs
@@ -0,0 +1,31 @@
+﻿// Leetcode 300
+
+int[] lst = { 10, 9, 2, 5, 3, 7, 101, 18 }; // Returns 4
+Console.WriteLine(LengthOfLIS(lst));
+
+static int LengthOfLIS(int[] nums)
+{
+    List<int> LIS = new List<int>(nums.Length);
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        LIS.Add(1);
+    }
+
+    for (int i = nums.Length - 1; i >= 0; i--)
+    {
+        for (int j = i + 1; j < nums.Length; j++)
+        {
+            if (nums[i] < nums[j])
+            {
+                LIS[i] = Math.Max(LIS[i], 1 + LIS[j]);
+            }
+        }
+    }
+
+    return LIS.Max();
+}
+
+
+
+
