94623510ae7221453665cb342b7b21b8cfcea627
dkohn <douglasjkohn@gmail.com>
2022-09-12 16:22:07 -0500

    Added LC121 in Csharp

---
+++ LC121/LC121_Csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿// LC 121
+// Best Time to Buy and Sell Stock
+
+using System;
+
+static int MaxProfit(int[] prices) 
+{
+    int l = 0;
+    int r = 0;
+    int maxP = 0;
+    int profit = 0;
+     while (r < prices.Length)
+    {
+        if (prices[l] < prices[r])
+        {
+            profit = prices[r] - prices[l];
+            maxP = Math.Max(maxP, profit);
+        }
+        else
+        {
+            l = r;
+        }
+        r++;
+    }
+
+    return maxP;
+}
+
+int[] prices = { 7, 1, 5, 3, 6, 4 };
+
+int result = MaxProfit(prices);
+Console.WriteLine(result);
