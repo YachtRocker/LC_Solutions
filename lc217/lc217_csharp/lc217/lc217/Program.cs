f9eed91fff33b5e6e99415e3aa5319fe3f83306c
dkohn <douglasjkohn@gmail.com>
2022-09-24 13:07:26 -0500

    Added LC217 in C#

--- lc217/lc217_csharp/lc217/lc217/Program.cs
+++ lc217/lc217_csharp/lc217/lc217/Program.cs
@@ -1,2 +1,30 @@
 ﻿// LC 217
-Console.WriteLine("Hello, World!");
+// Contains Duplicate
+
+using System;
+
+static bool ContainsDuplicate(int[] nums) {
+    int l = 0;
+    int r = 1;
+
+    Array.Sort(nums);
+
+    while (r < nums.Length)
+    {
+        if (nums[l] == nums[r])
+        {
+            return true;
+        }
+        else
+        {
+            l += 1;
+            r += 1;
+        }        
+    }
+
+    return false;
+}
+
+int[] nums = { 1,1,1,3,3,4,3,2,4,2 };
+
+Console.WriteLine(ContainsDuplicate(nums));
